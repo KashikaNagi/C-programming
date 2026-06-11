5d14bac12e5248a44dab65fb425ecf6646eb4646
Kashika Nagi <NAGIKASHIKA@GMAIL.COM>
2026-06-11 18:56:02 +0530

    added perimeter file

---
+++ perimeter_rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//perimeter of rectangle with sides a&b user input
+int main(){
+ int length,breadth,perimeter;
+ printf("enter length");
+ scanf("%d",&length);
+ printf("enter breadth");
+ scanf("%d",&breadth);
+ printf("perimeter is:%d", 2 * (length + breadth));
+return 0;
+}
+
+
