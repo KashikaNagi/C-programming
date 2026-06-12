2dd6f933ec3ea6eccf60e8558ed56021b729cc60
Kashika Nagi <NAGIKASHIKA@GMAIL.COM>
2026-06-12 21:53:15 +0530

    logic program

---
+++ usinglogic.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+//if its Sundaay and its Snowing print true or false for the following statement
+int main(){
+    int isSunday=1;
+    int isSnowing=1;
+printf("%d,\n", isSunday && isSnowing);
+return 0;
+} 
