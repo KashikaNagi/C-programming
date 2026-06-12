27d23c6cd7d9c59df1bc75bee9cce71289d6a72b
Kashika Nagi <NAGIKASHIKA@GMAIL.COM>
2026-06-12 21:58:59 +0530

    or program

---
+++ usingor.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+//if its Monday OR its Raining print true or false for the following statement
+int main(){
+    int isMonday=0;
+    int isRaining=1;
+printf("%d,\n", isMonday && isRaining);
+return 0;
+} 
