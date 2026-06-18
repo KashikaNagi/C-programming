41c694732387bf43190d4799d198fd0bcb6a8dc7
Kashika Nagi <NAGIKASHIKA@GMAIL.COM>
2026-06-18 22:09:01 +0530

    taking counter as character using for loop

---
+++ counter_ascharacterusingforloop.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+// taking counter as charact and printing a to z
+int main(){
+ //iterator;counter
+    for(char ch= 'a'; ch<='z'; ch++){
+        printf(" %c \n", ch);
+  }
+    return 0;
+}
