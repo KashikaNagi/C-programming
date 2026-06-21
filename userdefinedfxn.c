7d1602402bc1bd6ac26d57a983d619aefa2fccd1
Kashika Nagi <NAGIKASHIKA@GMAIL.COM>
2026-06-21 22:04:58 +0530

    add a program to print "Hello, World!"

---
+++ userdefinedfxn.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+//declaration/prototype of the function
+void printhello();
+int main(){
+    printhello(); // function call
+    return 0;
+}
+//definition of the function
+void printhello(){
+    printf("Hello, World!\n");
+
+} 
